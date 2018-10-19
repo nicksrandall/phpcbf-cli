db08e97441aa23afffa5c66abcd799811c5efbfb
Nick Randall <nicksrandall@gmail.com>
2018-10-19 14:29:27 -0400

    Clean up the code a bit

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
 
-new Promise(function(resolve, reject) {
-  const phpcbf = spawn('phpcbf', process.argv.slice(2));
-  phpcbf.stdout.on('data', data => {
-    console.log(`${data}`);
-  });
-  phpcbf.stderr.on('data', data => {
-    console.log(`stderr: ${data}`);
-  });
-  phpcbf.on('close', code => {
-    if (code > 1) {
-      reject(
-        new Error(
-          `There was a problem fixing coding problems. Returned exit code: ${code}`
-        )
-      );
-      return;
-    }
-    resolve(code);
-  });
+new Promise((resolve, reject) => {
+  const phpcbf = spawn("phpcbf", process.argv.slice(2));
+  phpcbf.stdout.on("data", data => console.log(data));
+  phpcbf.stderr.on("data", data => console.log(`stderr: ${data}`));
+  phpcbf.on("close", code => (code < 2 ? resolve(code) : reject(code)));
 })
-  .then(() => {
-    process.exit(0);
-  })
-  .catch(() => {
-    process.exit(1);
-  });
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1));
