2bdc3251c316e769b9b2d20df907fa426dcee5a4
Nick Randall <nicksrandall@gmail.com>
2019-03-26 11:03:01 -0400

    [NR] Fixing module

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const { spawn } = require("child_process");
+const {spawn} = require('child_process');
 
 new Promise((resolve, reject) => {
-  const phpcbf = spawn("phpcbf", process.argv.slice(2));
-  phpcbf.stdout.on("data", data => console.log(data));
-  phpcbf.stderr.on("data", data => console.log(`stderr: ${data}`));
-  phpcbf.on("close", code => (code < 2 ? resolve(code) : reject(code)));
+  const phpcbf = spawn('phpcbf', process.argv.slice(2));
+  phpcbf.stdout.on('data', data => console.log(data.toString()));
+  phpcbf.stderr.on('data', data => console.log(`stderr: ${data}`));
+  phpcbf.on('close', code => (code < 2 ? resolve(code) : reject(code)));
 })
   .then(() => process.exit(0))
   .catch(() => process.exit(1));
