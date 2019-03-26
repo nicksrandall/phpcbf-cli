0aec7c9a6caef90f87f6f33a3e7f331957922ed4
Nick Randall <nicksrandall@gmail.com>
2019-03-26 11:17:05 -0400

    Use local version of php code format

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const {spawn} = require('child_process');
 
 new Promise((resolve, reject) => {
-  const phpcbf = spawn('phpcbf', process.argv.slice(2));
+  const args = [path.resolve(__dirname, './phpcbf.phar')].concat(
+    process.argv.slice(2),
+  );
+  const phpcbf = spawn('php', args);
   phpcbf.stdout.on('data', data => console.log(data.toString()));
   phpcbf.stderr.on('data', data => console.log(`stderr: ${data}`));
   phpcbf.on('close', code => (code < 2 ? resolve(code) : reject(code)));
